8a44ddf7e87d8a051e63b1add99b12fc20916924
Jan Iwanow <iwanow.jan@gmail.com>
2013-03-05 09:28:39 +1100

    Added a little docs to IClosureTable interface

--- iclosuretable.php
+++ iclosuretable.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Database design pattern realization for Laravel.
+ *
+ * @package    ClosureTable
+ * @author     Jan Iwanow <iwanow.jan@gmail.com>
+ * @copyright  2013 Jan Iwanow
+ * @licence    MIT License <http://www.opensource.org/licenses/mit>
+ */
 namespace ClosureTable;
 
+/**
+ * Generic ClosureTable model interface. Provides strong type check.
+ */
 interface IClosureTable {}
