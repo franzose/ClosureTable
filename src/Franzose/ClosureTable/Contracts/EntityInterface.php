a5c873688a8c08de6d9a7f478583e522dc5e991d
Jan Iwanow <iwanow.jan@gmail.com>
2014-01-23 16:15:48 +1100

    added new methods to EntityInterface

--- src/Franzose/ClosureTable/Contracts/EntityInterface.php
+++ src/Franzose/ClosureTable/Contracts/EntityInterface.php
@@ -10,6 +10,20 @@ interface EntityInterface {
     const POSITION = 'position';
 
     /**
+     * Indicates whether the model has children.
+     *
+     * @return bool
+     */
+    public function isParent();
+
+    /**
+     * Indicates whether the model has no ancestors.
+     *
+     * @return bool
+     */
+    public function isRoot();
+
+    /**
      * @param EntityInterface $ancestor
      * @param int $position
      * @return EntityInterface
