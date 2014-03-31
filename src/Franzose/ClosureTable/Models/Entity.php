518a430073f5c76da32f21e258eaeeb30108f0ef
Tom Rochette <roctom@gmail.com>
2014-03-30 23:04:39 -0400

    Fix broken test by reverting change b71a2ac12bfc3f9249666a8664dffcaff51f286f.

--- src/Franzose/ClosureTable/Models/Entity.php
+++ src/Franzose/ClosureTable/Models/Entity.php
@@ -1295,28 +1295,25 @@ class Entity extends Eloquent implements EntityInterface {
      */
     protected function reorderSiblings($parentIdChanged = false)
     {
-        if ( ! is_null($this->old_position))
+        list($range, $action) = $this->setupReordering($parentIdChanged);
+
+        $positionColumn = $this->getPositionColumn();
+
+        // As the method called twice (before moving and after moving),
+        // first we gather "old" siblings by the old parent id value of the model.
+        if ($parentIdChanged === true)
         {
-            list($range, $action) = $this->setupReordering($parentIdChanged);
+            $query = $this->siblings(false, $this->old_parent_id);
+        }
+        else
+        {
+            $query = $this->siblings();
+        }
 
-            $positionColumn = $this->getPositionColumn();
-
-            // As the method called twice (before moving and after moving),
-            // first we gather "old" siblings by the old parent id value of the model.
-            if ($parentIdChanged === true)
-            {
-                $query = $this->siblings(false, $this->old_parent_id);
-            }
-            else
-            {
-                $query = $this->siblings();
-            }
-
-            if($action) {
-                $query->buildWherePosition($positionColumn, $range)
-                    ->where($this->getKeyName(), '<>', $this->getKey())
-                    ->$action($positionColumn);
-            }
+        if($action) {
+            $query->buildWherePosition($positionColumn, $range)
+                ->where($this->getKeyName(), '<>', $this->getKey())
+                ->$action($positionColumn);
         }
     }
 
