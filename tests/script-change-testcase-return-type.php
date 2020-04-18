9dbc35d0b591411682715bdeb94bbbe5a57abf8e
Jan Iwanow <iwanow.jan@gmail.com>
2020-04-18 08:34:39 +0700

    refactored Travis CI workaround script

--- tests/script-change-testcase-return-type.php
+++ tests/script-change-testcase-return-type.php
@@ -1,8 +1,26 @@
-<?php 
+<?php
+// This file patches test case files containing setUp and tearDown methods.
+// It's a hacky workaround needed until we drop support of PHP < 7.2.
+// Cause: newer PHPUnit has added typehints to its setUp and tearDown methods
+// but ClosureTable 6.x must support PHP 5.6-7.0 as well,
+// so we cannot use typehints by default.
 
-if(version_compare(phpversion(), '7.1', '<')){
+if (PHP_VERSION_ID < 70100) {
     exit(0);
 }
 
-file_put_contents('tests/BaseTestCase.php', str_replace('public function setUp()', 'public function setUp(): void', file_get_contents('tests/BaseTestCase.php')));
-file_put_contents('tests/BaseTestCase.php', str_replace('public function tearDown()', 'public function tearDown(): void', file_get_contents('tests/BaseTestCase.php')));
+$filesToPatch = [
+    'tests/BaseTestCase.php',
+    'tests/Console/MakeCommandTests.php'
+];
+
+foreach ($filesToPatch as $path) {
+    $contents = file_get_contents($path);
+    $contents = str_replace(
+        ['public function setUp()', 'public function tearDown()'],
+        ['public function setUp(): void', 'public function tearDown(): void'],
+        $contents
+    );
+
+    file_put_contents($path, $contents);
+}
